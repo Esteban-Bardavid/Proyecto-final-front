191bc956faa7d8c197d0b01c617b907dc51beac2
Isel Salomé Esper <rodrigoesper94@gmail.com>
2023-01-12 10:08:04 -0300

    cambio de clases modificacion de boostrap

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,34 +12,43 @@ function NavBarComponent() {
 
     return (
         <div>
-            <div className=" d-flex justify-content-center">
+            <div className=" d-flex justify-content-center mb-0">
                 <h2>3 <small>CUOTAS SIN INTERES </small><strong>-ENVIOS GRATIS</strong> <small>DESDE $20.000</small></h2>
             </div>
-            <Navbar className="navContainer" expand="lg">
-                <Container fluid>
-                    <Navbar.Brand href="#">ROLLING SHOES</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
 
-                        <Form className="d-flex ">
-                            <Form.Control
-                                type="search"
-                                placeholder="Adidas, Nike"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-success">Buscar</Button>
-                        </Form>
+            <div className="navbar-container">
+                <Navbar className="navContainer" expand="lg">
 
-                        <Nav
-                        >
-                            <Nav.Link href="#action1">Home</Nav.Link>
-                            <Nav.Link href="#action2">carrito</Nav.Link>
-                            <Nav.Link href="#action2">perfil</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                    <div className="d-flex juatify-content-center " >
+                        <Navbar.Brand href="#">ROLLING SHOES</Navbar.Brand>
+
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <div className="form-box">
+                                <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Adidas, Nike"
+                                        className="me-2"
+                                        aria-label="Search" />
+                                    <Button variant="outline-success">Buscar</Button>
+                                </Form>
+                            </div>
+                            <div className="nav-links">
+                                <Nav>
+                                    <Nav.Link href="#action1">HomeIcons</Nav.Link>
+                                    <Nav.Link href="#action2">Destacados</Nav.Link>
+                                    <Nav.Link href="#action3">CarritoIcons</Nav.Link>
+                                    <Nav.Link href="#action4">Contacto</Nav.Link>
+                                    <Nav.Link href="#action5">Perfil</Nav.Link>
+                                    <Nav.Link href="#action6">Ayuda</Nav.Link>
+                                </Nav>
+                            </div>
+                        </Navbar.Collapse>
+                    </div>
+
+                </Navbar>
+            </div>
         </div>
     )
 }
