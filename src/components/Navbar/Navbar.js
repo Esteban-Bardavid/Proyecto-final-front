f776e6acbdcb1b9d9aed442bff65cc730f3311c0
Isel Salomé Esper <rodrigoesper94@gmail.com>
2023-01-12 08:08:42 -0300

    se agrega navbar, nav, imput de busqueda

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import "./Navbar.css";
-import { Container, Nav, Navbar, Button } from 'react-bootstrap';
+import { Container, Nav, Navbar, Button, NavDropdown, Form } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 function NavBarComponent() {
@@ -12,7 +12,34 @@ function NavBarComponent() {
 
     return (
         <div>
-           hola soy el navbar
+            <div className=" d-flex justify-content-center">
+                <h2>3 <small>CUOTAS SIN INTERES </small><strong>-ENVIOS GRATIS</strong> <small>DESDE $20.000</small></h2>
+            </div>
+            <Navbar className="navContainer" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">ROLLING SHOES</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+
+                        <Form className="d-flex ">
+                            <Form.Control
+                                type="search"
+                                placeholder="Adidas, Nike"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Buscar</Button>
+                        </Form>
+
+                        <Nav
+                        >
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">carrito</Nav.Link>
+                            <Nav.Link href="#action2">perfil</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     )
 }
