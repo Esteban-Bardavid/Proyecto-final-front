1297513ad3358e11d78e72e570953105fc312212
Hugo Alderete <hugoaalderete7@gmail.com>
2023-01-27 10:53:42 -0300

    Esta rama esta igual como la baje del GitHub

--- src/utils/useProducts.js
+++ src/utils/useProducts.js
@@ -6,36 +6,36 @@ function useProducts() {
 
   const productos = products;
   const [Filter, SetFilter] = useState(productos);
-  const [Search, SetSearch] = useState ('');
+  const [Search, SetSearch] = useState('');
 
   const handleFilter = (e) => {
     const filterValue = e.target.value;
     SetSearch(filterValue);
 
-    
-    if (filterValue != Search){
+
+    if (filterValue !== Search) {
       if (filterValue === 'Nike') {
         const filteredProducts = productos.filter(
           (item) => item.marca === 'Nike');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Adidas') {
         const filteredProducts = productos.filter(
           (item) => item.marca === 'Adidas');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Fila') {
         const filteredProducts = productos.filter(
           (item) => item.marca === 'Fila');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Reebok') {
         const filteredProducts = productos.filter(
           (item) => item.marca === 'Reebok');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Todas las marcas') {
@@ -44,41 +44,41 @@ function useProducts() {
       else if (filterValue === 'Running') {
         const filteredProducts = productos.filter(
           (item) => item.categoria === 'Running');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Footbal') {
         const filteredProducts = productos.filter(
           (item) => item.categoria === 'Footbal');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Trainning') {
         const filteredProducts = productos.filter(
           (item) => item.categoria === 'Trainning');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Outdoor') {
         const filteredProducts = productos.filter(
           (item) => item.categoria === 'Trainning');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Clasicas') {
         const filteredProducts = productos.filter(
           (item) => item.categoria === 'Clasicas');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Ojotas') {
         const filteredProducts = productos.filter(
           (item) => item.categoria === 'Ojotas');
-  
+
         SetFilter(filteredProducts)
       }
       else if (filterValue === 'Todas las marcas') {
-  
+
         SetFilter(productos)
       }
       else (
@@ -88,9 +88,9 @@ function useProducts() {
     else (
       SetFilter(productos)
     )
-   
 
-   
+
+
     console.log(productos)
   };
   return {
