88e6a932e329221c765e34678c77d690272c3ea5
Hugo Alderete <hugoaalderete7@gmail.com>
2023-02-12 13:31:41 -0300

    AdminProducts + Carrito

--- src/pages/AdminProducts/AdminProductsPage.js
+++ src/pages/AdminProducts/AdminProductsPage.js
@@ -6,72 +6,93 @@ import axios from 'axios';
 import { FaTrashAlt } from "react-icons/fa";
 import ModalUpProducts from '../../components/Modal/modalUpProducts';
 
+let styles = {
+    fontWeight: "bold",
+    color: "#dc3545"
+}
+
 
 function AdminProducts() {
 
-    const { OnChangeProducts, CreateProducts, handleClose, handleShow, show, url, DeleteProducts } = UseAdminProducts();
+    const {
+        handleBlur,
+        OnChangeProducts,
+        form,
+        errors,
+        handleClose,
+        handleShow,
+        show,
+        url,
+        CreateProducts,
+        DeleteProducts,
+    } = UseAdminProducts();
+
 
     function Auxiliar() {
         CreateProducts();
         handleClose();
     }
 
-       // Funcion para mostrar productos en tabla:
-       const [products, setproducts] = useState([]);
-    
-       useEffect(() => {
-           GetProducts()
-       }, []);
-       
-       async function GetProducts() {
-           let getProducts = await axios.get(`${url}/adminProducts`);
-           setproducts(getProducts.data);
-           setBuscador(getProducts.data);
-       }
-       
-       // Buscador:
-       const [busqueda, setBusqueda] = useState ('')
-       const [buscador, setBuscador] = useState ([])
-       
-       const handleChange = e => {
-           setBusqueda (e.target.value)
-           filtrar(e.target.value)
-       }
-       
-       const filtrar = (terminoBusqueda) => {
-           var response = buscador.filter((elemento)=>{
-               if(elemento.producto.includes(terminoBusqueda)
-               ||elemento.marca.includes(terminoBusqueda)
-               ||elemento.sex.includes(terminoBusqueda)
-               ){
-                   return elemento;
-               }
-           });
-           setproducts(response);
-           console.log (products);
-       }
+    // Funcion para mostrar productos en tabla:
+    const [products, setproducts] = useState([]);
+
+    useEffect(() => {
+        GetProducts()
+    }, []);
+
+    async function GetProducts() {
+        let getProducts = await axios.get(`${url}/adminProducts`);
+        setproducts(getProducts.data);
+        setBuscador(getProducts.data);
+    }
+
+    // Buscador:
+    const [busqueda, setBusqueda] = useState('')
+    const [buscador, setBuscador] = useState([])
+
+    const handleChange = e => {
+        setBusqueda(e.target.value)
+        filtrar(e.target.value)
+    }
+
+    const filtrar = (terminoBusqueda) => {
+        var response = buscador.filter((elemento) => {
+            if (elemento.producto.toLowerCase().includes(terminoBusqueda.toLowerCase())
+                || elemento.marca.toLowerCase().includes(terminoBusqueda.toLowerCase())
+                || elemento.sex.toLowerCase().includes(terminoBusqueda.toLowerCase())
+            ) {
+                return elemento;
+            }
+        });
+        setproducts(response);
+        console.log(products);
+    }
+
+
 
     return (
         <div>
-            <div className='mb-2 ms-5 d-flex justify-content-start'>
-                <Button variant="primary" onClick={handleShow}>
-                    Ingresar Producto
-                </Button>
+            <div className=''>
+                <div className='ms-4 mt-3'>
+                    <Button variant="primary" onClick={handleShow}>
+                        Ingresar Producto
+                    </Button>
+                </div>
+
+                <div className='d-flex justify-content-center w-100 ms-2' >
+                    <input
+                        className='form-control w-75 mb-3 mt-3 me-3'
+                        value={busqueda}
+                        placeholder='Buscar'
+                        onChange={handleChange}
+                    />
+                    <Button className='btn btn-primary mb-3 mt-3'>
+                        <FaSearch />
+                    </Button>
+                </div>
             </div>
 
-            <div className='d-flex justify-content-center'>
-                <input
-                className='form-control w-50 mb-4'
-                value={busqueda}
-                placeholder='Buscar'
-                onChange={handleChange}
-                />
-                <Button className='btn btn-primary me-3 ms-2 mb-4'>
-                    <FaSearch/>
-                </Button>
-            </div>
-
-{/* ---------------------------------------------------------- Tabla --------------------------------------------------------------------------- */}
+            {/* ---------------------------------------------------------- Tabla --------------------------------------------------------------------------- */}
 
             <Table striped bordered hover responsive className='w-100'>
                 <thead>
@@ -144,7 +165,7 @@ function AdminProducts() {
                 </tbody>
             </Table>
 
-{/* ---------------------------------------------------------- Modal Ingresar Producto -------------------------------------------------- */}
+            {/* ---------------------------------------------------------- Modal Ingresar Producto -------------------------------------------------- */}
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
@@ -162,7 +183,11 @@ function AdminProducts() {
                                     name='producto'
                                     type='text'
                                     onChange={OnChangeProducts}
-                                    placeholder="nombre" />
+                                    onBlur={handleBlur}
+                                    value={form.producto}
+                                    placeholder="nombre"
+                                />
+                                {errors.producto && <p style={styles}>{errors.producto}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 px-2 w-50" controlId="formBasicEmail">
@@ -171,7 +196,10 @@ function AdminProducts() {
                                     name='imgUrl'
                                     type='text'
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.imgUrl}
                                     placeholder='path o url' />
+                                {errors.imgUrl && <p style={styles}>{errors.imgUrl}</p>}
                             </Form.Group>
                         </div>
 
@@ -182,7 +210,10 @@ function AdminProducts() {
                                     name='sex'
                                     type="text"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.sex}
                                     placeholder="M / H" />
+                                {errors.sex && <p style={styles}>{errors.sex}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 px-2 w-25" controlId="formBasicEmail">
@@ -191,7 +222,10 @@ function AdminProducts() {
                                     name='marca'
                                     type="text"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.marca}
                                     placeholder="Adidas" />
+                                {errors.marca && <p style={styles}>{errors.marca}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 px-2 w-25" controlId="formBasicEmail">
@@ -200,7 +234,10 @@ function AdminProducts() {
                                     name='color'
                                     type="text"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.color}
                                     placeholder="Negro" />
+                                {errors.color && <p style={styles}>{errors.color}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 px-2 w-25" controlId="formBasicEmail">
@@ -209,7 +246,10 @@ function AdminProducts() {
                                     name='precio'
                                     type="number"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.precio}
                                     placeholder="100.00" />
+                                {errors.precio && <p style={styles}>{errors.precio}</p>}
                             </Form.Group>
                         </div>
 
@@ -220,7 +260,10 @@ function AdminProducts() {
                                     name='tseis'
                                     type="number"
                                     onChange={OnChangeProducts}
-                                    placeholder="Cant."/>
+                                    onBlur={handleBlur}
+                                    value={form.tseis}
+                                    placeholder="Cant." />
+                                {errors.tseis && <p style={styles}>{errors.tseis}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 w-25" controlId="formBasicEmail">
@@ -229,7 +272,10 @@ function AdminProducts() {
                                     name='tsiete'
                                     type="number"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.tsiete}
                                     placeholder="Cant." />
+                                {errors.tsiete && <p style={styles}>{errors.tsiete}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 px-2 w-25" controlId="formBasicEmail">
@@ -238,7 +284,10 @@ function AdminProducts() {
                                     name='tocho'
                                     type="number"
                                     onChange={OnChangeProducts}
-                                    placeholder="Cant."/>
+                                    onBlur={handleBlur}
+                                    value={form.tocho}
+                                    placeholder="Cant." />
+                                {errors.tocho && <p style={styles}>{errors.tocho}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 w-25" controlId="formBasicEmail">
@@ -247,7 +296,10 @@ function AdminProducts() {
                                     name='tnueve'
                                     type="number"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.tnueve}
                                     placeholder="Cant." />
+                                {errors.tnueve && <p style={styles}>{errors.tnueve}</p>}
                             </Form.Group>
                         </div>
 
@@ -258,7 +310,10 @@ function AdminProducts() {
                                     name='ccero'
                                     type="number"
                                     onChange={OnChangeProducts}
-                                    placeholder="Cant."/>
+                                    onBlur={handleBlur}
+                                    value={form.ccero}
+                                    placeholder="Cant." />
+                                {errors.ccero && <p style={styles}>{errors.ccero}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 w-25" controlId="formBasicEmail">
@@ -267,7 +322,10 @@ function AdminProducts() {
                                     name='cuno'
                                     type="number"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.cuno}
                                     placeholder="Cant." />
+                                {errors.cuno && <p style={styles}>{errors.cuno}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 px-2 w-25" controlId="formBasicEmail">
@@ -276,7 +334,10 @@ function AdminProducts() {
                                     name='cdos'
                                     type="number"
                                     onChange={OnChangeProducts}
-                                    placeholder="Cant."/>
+                                    onBlur={handleBlur}
+                                    value={form.cdos}
+                                    placeholder="Cant." />
+                                {errors.cdos && <p style={styles}>{errors.cdos}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 w-25" controlId="formBasicEmail">
@@ -285,7 +346,10 @@ function AdminProducts() {
                                     name='ctres'
                                     type="number"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.ctres}
                                     placeholder="Cant." />
+                                {errors.ctres && <p style={styles}>{errors.ctres}</p>}
                             </Form.Group>
                         </div>
 
@@ -296,7 +360,10 @@ function AdminProducts() {
                                     name='categoria'
                                     type="text"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.categoria}
                                     placeholder="Trainning - Outdoor - Running" />
+                                {errors.categoria && <p style={styles}>{errors.categoria}</p>}
                             </Form.Group>
 
                             <Form.Group className="mb-2 w-25" controlId="formBasicEmail">
@@ -305,7 +372,10 @@ function AdminProducts() {
                                     name='publicado'
                                     type="number"
                                     onChange={OnChangeProducts}
+                                    onBlur={handleBlur}
+                                    value={form.publicado}
                                     placeholder="1 / 0" />
+                                {errors.publicado && <p style={styles}>{errors.publicado}</p>}
                             </Form.Group>
                         </div>
                     </Form>
