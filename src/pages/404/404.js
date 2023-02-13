0562d0f3633eb6acaa644d156a61402bf7f6ffbd
Esteban-Bardavid <Bardavidesteban@gmail.com>
2023-02-12 23:41:03 -0300

    error 404

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,10 +1,22 @@
 import "./404.css";
 
+
 function ErrorPage() {
     return (
-        <div>
-            <h1>404</h1>
+        
+        <body className="BodyConatiner">
+
+        <div className="container">
+            <img src="https://raw.githubusercontent.com/isai-ismael/error-404/3733d9b9b8e876ef1adbd075a2061fd52d713ded/img/img1.svg" alt="imagen de error 404"></img>
+            <h1>La página que ha solicitado no se encuentra disponible</h1>
+            <div className="boton">
+            <button type="button" class="btn btn-primary btn-lg">Volver atras</button>
+            </div>
         </div>
+    
+    
+    
+    </body>
     )
 }
 
